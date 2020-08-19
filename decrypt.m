bc8b5cdc43e4dd40252ca588d072ef38dafa3cd8
AhmedSalah339 <59888340+AhmedSalah339@users.noreply.github.com>
2020-08-19 21:55:49 +0200

    Add files via upload

---
+++ decrypt.m
@@ -0,0 +1,44 @@
+function decrypt(file_name,n,d,n_bits,n_bits2,k0)
+%read the encryption
+fileID = fopen(join([file_name,'_encrypted.txt']),'r');
+c = fscanf(fileID,'%c');
+fclose(fileID);
+%get the numbers
+c = split(c);
+for i =1:length(c)
+k = vpi(c{i});
+if length(k) ~=0
+    c{i}=k;
+end
+end
+
+%get the decoded messege and remove the additional spaces from the OAEP
+%using the num of bits
+
+%note: The number of bits is a feature of the encoding system for sure the n_bits2 
+%is not a number that is shared between the two sides but it is simply the
+%first number divisible by 8 smaller than n_bits which you can calculate
+%like in the encrypt function
+
+dec_mess = '';
+num_spaces = ceil(mod(2*n_bits2,n_bits)/8);
+for i =1:length(c)-1
+%decode the chuncks and make the messege
+k = expen(c{i},d,n);
+k = OEABdec(k,n_bits,k0);
+k = char(k);
+k = k(num_spaces+1:end);
+dec_mess = join([dec_mess,k]);
+end
+%write the results
+name = join([file_name,'_decrypted.txt']);
+filepath = 'C:\Users\User\Desktop\Discrete\project';
+filepath = fullfile(filepath, name);
+
+[fid, msg] = fopen(filepath,'w');
+if fid == -1
+  error(['Cannot open file: %s', char(10), '  %s'], filepath, msg);
+end
+fprintf(fileID,'%c',char(dec_mess));
+fclose(fileID);
+end
---
+++ encrypt.m
@@ -0,0 +1,45 @@
+function n_bits = encrypt(file_name,n,e,k0)
+% read the messege
+fileID = fopen(join([file_name,'.txt']),'r');
+M = fscanf(fileID,'%c');
+fclose(fileID);
+%get the closest number divisible by 8 to n_bits
+n2 = vpi2bin(n);
+for i=length(n2):-1:1
+    if mod(i,8)==0
+n_bits = i;
+break;
+    end
+end
+
+Ms=chunck(M,n_bits-k0);
+%encrypt the chuncks and integrate them in one string
+C ='';
+for i=1:length(Ms)
+[c1,m] = OEABenc(Ms{i},n_bits,k0);
+
+m = expen(m,e,n);
+%retrive the number from the vpi as array and convert it to string
+m = struct(m);
+m = m.digits;
+m = m(length(m):-1:1);
+m = num2str(m);
+m = m(find(~isspace(m)));
+C = join([C,m]);
+C = join([C,' ']);
+
+end
+
+%write the results
+name = join([file_name,'_encrypted.txt']);
+filepath = 'C:\Users\User\Desktop\Discrete\project';
+filepath = fullfile(filepath, name);
+
+[fid, msg] = fopen(filepath,'w');
+if fid == -1
+  error(['Cannot open file: %s', char(10), '  %s'], filepath, msg);
+end
+
+fprintf(fileID,'%c',C);
+fclose(fileID);
+end
